a19561cd84e29b9f02c17fb7d3a7187740671a50
abogazee515-droid <abogazee515@gmail.com>
2026-05-19 10:59:02 +0700

    Test polymorphism pointer

--- abstraksi/Polymorphism.cpp
+++ abstraksi/Polymorphism.cpp
@@ -24,3 +24,19 @@ public:
         cout << "Pesan dari lia" << endl;
     }
 };
+
+int main() {
+
+    seseorang* obyek;
+
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+
+    obyek = &b;
+    obyek->pesan();
+
+    return 0;
+}
